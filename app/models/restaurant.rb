0bf997bf7accecf7202b7cc4e1cb317bc97141b9
Nate Hill <natanio@gmail.com>
2014-08-11 08:24:22 +0900

    Validate user data

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,4 +2,8 @@ class Restaurant < ActiveRecord::Base
   mount_uploader :image, ImageUploader
   
   has_many :reviews
+  
+  validates :name, :address, :phone, :website, :image, presence: true
+  validates :website, format: { with: /\Ahttps?:\/\/.*\z/,
+  	message: "must start with http:// or https://"}
 end
